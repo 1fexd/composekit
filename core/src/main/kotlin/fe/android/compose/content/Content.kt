0715d5add4d7bb569c59c5d5609c127ede2e04b6
1fexd <PGFvy85nC@protonmail.com>
2025-02-24 07:18:54 +0100

    fix: OptionalContent typealias

--- core/src/main/kotlin/fe/android/compose/content/Content.kt
+++ core/src/main/kotlin/fe/android/compose/content/Content.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 
 typealias Content = @Composable () -> Unit
-typealias OptionalContent = Content?
+typealias OptionalContent = (@Composable () -> Unit)?
 
 @Composable
 inline fun rememberOptionalContent(condition: Boolean, crossinline content: @Composable () -> Unit): OptionalContent {
