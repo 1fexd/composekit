33ab6e5ff83bee9f8fcc487a9f8fe7b49df66466
1fexd <PGFvy85nC@protonmail.com>
2025-05-17 18:09:30 +0200

    chore: Dependencies

--- span/test-app/src/main/java/fe/android/span/helper/SpanTextPreview.kt
+++ span/test-app/src/main/java/fe/android/span/helper/SpanTextPreview.kt
@@ -3,19 +3,15 @@ package fe.android.span.helper
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import fe.android.span.helper.composable.createAnnotatedString
 import fe.androidspanhelper.testapp.R
 
 @Composable
@@ -53,6 +49,6 @@ private fun SpanTextPreview() {
 private fun TestText(name: String, @StringRes id: Int, vararg formatArgs: Any?) {
     Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
         Text(text = name, fontWeight = FontWeight.Bold)
-        Text(text = createAnnotatedString(id = id, *formatArgs))
+//        Text(text = createAnnotatedString(id = id, *formatArgs))
     }
 }
