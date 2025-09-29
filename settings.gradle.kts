907203b88b55989c4b5741fe5b59f7d5539c30f4
1fexd <PGFvy85nC@protonmail.com>
2025-09-29 07:31:01 +0200

    chore: Dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,8 @@ pluginManagement {
     }
 
     plugins {
-        id("de.fayard.refreshVersions") version "0.60.5"
-////                                # available:"0.60.6"
+        id("de.fayard.refreshVersions") version "0.60.6"
         id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-////                                                       # available:"1.0.0-rc-1"
-////                                                       # available:"1.0.0"
         id("net.nemerosa.versioning")
         id("com.android.library")
         id("org.jetbrains.kotlin.android")
