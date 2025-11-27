cec7042d1d215d2eeaffa466285693d4473f965c
1fexd <PGFvy85nC@protonmail.com>
2025-11-27 07:03:40 +0100

    feat: Add ResolveInfo.packageName extension

--- modules/core/src/main/kotlin/fe/composekit/extension/ResolveInfoExt.kt
+++ modules/core/src/main/kotlin/fe/composekit/extension/ResolveInfoExt.kt
@@ -5,3 +5,8 @@ import android.content.pm.ResolveInfo
 
 public val ResolveInfo.info: ComponentInfo
     get() = activityInfo ?: providerInfo ?: serviceInfo
+
+
+public val ResolveInfo.packageName: String?
+    get() = resolvePackageName ?: info.packageName ?: info.applicationInfo.packageName
+
