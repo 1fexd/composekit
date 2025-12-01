3829ce98404bf8e49f3d49348c928a5faf0a32a8
1fexd <PGFvy85nC@protonmail.com>
2025-12-01 11:33:04 +0100

    chore: Adjust info extension

--- modules/core/src/main/kotlin/fe/composekit/extension/ResolveInfoExt.kt
+++ modules/core/src/main/kotlin/fe/composekit/extension/ResolveInfoExt.kt
@@ -2,11 +2,18 @@ package fe.composekit.extension
 
 import android.content.pm.ComponentInfo
 import android.content.pm.ResolveInfo
+import android.util.StringBuilderPrinter
 
 public val ResolveInfo.info: ComponentInfo
-    get() = activityInfo ?: providerInfo ?: serviceInfo
+    get() = activityInfo ?: serviceInfo ?: providerInfo
 
 
-public val ResolveInfo.packageName: String?
+public val ResolveInfo.packageName: String
     get() = resolvePackageName ?: info.packageName ?: info.applicationInfo.packageName
 
+public fun ResolveInfo.toStringBuilder(sb: StringBuilder = StringBuilder()): StringBuilder {
+    val sbp = StringBuilderPrinter(sb)
+    dump(sbp, "")
+
+    return sb
+}
