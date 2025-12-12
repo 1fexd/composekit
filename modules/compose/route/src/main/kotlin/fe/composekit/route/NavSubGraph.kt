0af4f611ff57aa46e4cca9828b6033f4ac51427f
1fexd <PGFvy85nC@protonmail.com>
2025-12-12 18:13:18 +0100

    fix: nav

--- modules/compose/route/src/main/kotlin/fe/composekit/route/NavSubGraph.kt
+++ modules/compose/route/src/main/kotlin/fe/composekit/route/NavSubGraph.kt
@@ -6,14 +6,17 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.navigation
-import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.starProjectedType
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-public interface NavSubGraph<R : Route> {
-    public val startDestination: KClass<R>
+public interface NavSubGraph<out R : Route> {
+    public val startDestination: R
+    public val graph: NavGraphBuilder.(NavHostController) -> Unit
+}
+
+public interface Nav {
     public val graph: NavGraphBuilder.(NavHostController) -> Unit
 }
 
@@ -27,6 +30,11 @@ public inline fun <R : Route, reified G : NavSubGraph<R>> NavGraphBuilder.attach
     ) {
         page.graph(this, navController)
     }
+}
+
+
+public inline fun <reified N : Nav> NavGraphBuilder.attachNav(nav: N, navController: NavHostController) {
+    nav.graph(this, navController)
 }
 
 public object NavTypes {
