73078548507e9d33495709384e8a579eb1a2b2cc
1fexd <PGFvy85nC@protonmail.com>
2025-12-12 15:49:39 +0100

    chore: Add missing type

--- modules/lifecycle/koin/src/main/kotlin/fe/android/lifecycle/koin/extension/ExtendedScopeExt.kt
+++ modules/lifecycle/koin/src/main/kotlin/fe/android/lifecycle/koin/extension/ExtendedScopeExt.kt
@@ -3,5 +3,5 @@ package fe.android.lifecycle.koin.extension
 import fe.android.lifecycle.LifecycleServiceRegistry
 import fe.droidkit.koin.ExtendedScope
 
-public val <T : Any> ExtendedScope<T>.applicationLifecycle
+public val <T : Any> ExtendedScope<T>.applicationLifecycle: LifecycleServiceRegistry
     get() = scope.get<LifecycleServiceRegistry>()
