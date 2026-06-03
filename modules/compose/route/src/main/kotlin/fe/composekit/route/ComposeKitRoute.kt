ed3eb756963cb0d8214552f48d89b3c7ec032347
1fexd <PGFvy85nC@protonmail.com>
2026-06-03 13:12:58 +0200

    chore: Allow injecting warmup dispatcher

--- modules/compose/route/src/main/kotlin/fe/composekit/route/ComposeKitRoute.kt
+++ modules/compose/route/src/main/kotlin/fe/composekit/route/ComposeKitRoute.kt
@@ -1,10 +1,13 @@
 package fe.composekit.route
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 public object ComposeKitRoute {
-    public suspend fun warmup(): Unit = withContext(Dispatchers.IO){
+    public suspend fun warmup(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ): Unit = withContext(dispatcher) {
         NavTypes.Types
     }
 }
